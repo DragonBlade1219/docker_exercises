7bede71fc31fbfb0e4e7df184373eaacb5485ddd
Andrés De La Torre <adelatorre@bside.com.mx>
2024-09-03 17:49:57 -0600

    [add] Se agregan archivos para práctica con Dcker compose.

---
+++ Compose/backend/api.py
@@ -0,0 +1,25 @@
+import json
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/getMyInfo')
+def get_my_info():
+    value = {
+        "name": "Amin",
+        "lastname": "Espinoza",
+        "socialMedia":
+        [
+            {"facebookUser": "andresdeatorrevilla"},
+            {"instagramUser": "andresdeatorrevilla"},
+            {"xUser": "andresdeatorrevilla"},
+            {"linkedin": "/adltv"},
+            {"githubUser": "DragonBlade"}
+        ],
+        "blog": "https://aandresdelatorrevilla.com",
+        "author": "Andrés De La Torre Villa"
+    }
+    return value
+
+if __name__ == '__main':
+    app.host(port=5000)
